df60a31a2c9c73bc7636ce1cb4ccef0316f54729
thedhruvkumar <dhruv.buxar@gmail.com>
2022-05-10 21:03:33 +0530

    PageNotFound Page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
-import { Signup } from "./components/Signup";
 import AuthState from "./context/auth/auth";
+import { BrowserRouter , Route , Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Signup } from "./components/Signup";
 import { Dashboard } from "./components/Dashboard";
 import {Login} from "./components/Login";
+import { NotFound } from "./components/NotFound";
+import { PostBox } from "./components/PostBox";
 
 function App() {
   return (
     <>
       <AuthState>
+        <BrowserRouter>
         <ToastContainer
           position="top-left"
           autoClose={5000}
@@ -19,11 +23,18 @@ function App() {
           pauseOnFocusLoss
           draggable
           pauseOnHover
-        />
-        {/* <Login/> */}
-        <Dashboard/>
-        <Signup/>
+          />
+          <Routes>
+
+           <Route exact path ="/" element={<Login/>} />
+           <Route exact path ="/dashboard" element={<Dashboard/>} />
+           <Route exact path ="/signup" element={<Signup/>} />
+           <Route exact path ="/posts" element={<PostBox/>} />
+           <Route exact path ="*" element={<NotFound/>} />
         
+          </Routes>
+        
+          </BrowserRouter>
       </AuthState>
     </>
   );
---
+++ client/src/components/PostBox.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const PostBox = () => {
+  return (
+    <>
+    <div>He</div>
+    </>
+  )
+}
