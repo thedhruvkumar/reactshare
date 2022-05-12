2409b5f6e0a002bdf325bfbd980c3c4120c31380
thedhruvkumar <dhruv.buxar@gmail.com>
2022-05-12 14:11:43 +0530

    Update PostBox.jsx

--- client/src/components/PostBox.jsx
+++ client/src/components/PostBox.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 export const PostBox = () => {
   return (
     <>
-    <div>He</div>
+    <div className='bg-red-600 w-screen h-screen'>
+        <div className="w-screen">
+            <input type="text" name="" id="" />
+        </div>
+    </div>
     </>
   )
 }
